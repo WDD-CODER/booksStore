af616b4e8322a42e8feeb377ae13c6898f2a1d1f
Your Name <dan.weibren@gmail.com>
2025-03-06 19:32:14 +0200

    Create function removebook

--- js/book.controller.js
+++ js/book.controller.js
@@ -1,19 +1,33 @@
 'use strict';
 
 function onInit() {
-    render()
+    console.log('gBooks', gBooks);
+    
+    render(gBooks)
 }
-function render() {
+function render(gBooks) {
+    if (!gBooks) {
+        console.log('empty');
+        return
+    }
+
     const elBody = document.querySelector('body')
     elBody.innerHTML = `<table> <tbody> <tr class="table-header"><th>title</th><th>price </th><th>action</th></tr></tbody></table>`
-    const elTbody = document.querySelector('table')
+    const elTbody = document.querySelector('tbody')
     gBooks.map(book => {
-        elTbody.innerHTML += `<td>book name</td>
-                              <td>price$</td>
+        elTbody.innerHTML += `<tr>
+                              <td>${book.title}</td>
+                              <td>${book.price}</td>
                               <td>
-                              <button class="read-button">read</button>
-                              <button class="update-button">update</button>
-                              <button class="delete-button">delete</button>   
-                              </td>`
+                              <button onclick="" class="read-button">read</button>
+                              <button onclick="" class="update-button">update</button>
+                              <button onclick="onRemoveBook('${book.title}')" class="delete-button">delete</button>   
+                              </td></tr>`
     })
+}
+
+function onRemoveBook(bookTitle) {
+    var idx =  gBooks.findIndex(book => book.title === bookTitle)
+    RemoveBook(idx)
+    render(gBooks)
 }
--- js/service/book.service.js
+++ js/service/book.service.js
@@ -4,9 +4,14 @@ var gBooks = getBooks()
 
 function getBooks(){
      gBooks = [
-        {title: 'blabla', price: '2$'},
-        {title: 'blabla', price: '2$'},
-        {title: 'blabla', price: '2$'}
+        {title: 'name1', price: '1$'},
+        {title: 'name2', price: '2$'},
+        {title: 'name3', price: '3$'}
     ]
     return gBooks
     }
+
+  function RemoveBook(idx){
+    gBooks.splice(idx, 1)
+  }
+  
