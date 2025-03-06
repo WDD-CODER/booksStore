7ff73df46f892c53e73f909a48cee6399977961b
Your Name <dan.weibren@gmail.com>
2025-03-06 23:35:09 +0200

    Fix book objects

--- js/service/book.service.js
+++ js/service/book.service.js
@@ -7,8 +7,8 @@ function getBooks() {
   gBooks = [
     { id: '1', title: 'name1', price: '1$', imgUrl: 'img/drawingTree.jpg' },
     { id: '2', title: 'name2', price: '2$', imgUrl: 'img/expirationDates.jpg' },
-    { id: '3', title: 'name3', price: '3$', imgUrl: 'img/happyPlace.jpg' }
-    { id: '3', title: 'name3', price: '3$', imgUrl: 'img/harryPotter.jpg' }
+    { id: '3', title: 'name3', price: '3$', imgUrl: 'img/happyPlace.jpg' },
+    { id: '3', title: 'name3', price: '3$', imgUrl: 'img/harryPotter.jpg' },
     { id: '3', title: 'name3', price: '3$', imgUrl: 'img/lionsGaze.jpg' }
   ]
   return gBooks
