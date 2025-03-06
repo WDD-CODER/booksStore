8bcb7eb6027d31637155f4fc8834def903f7661e
Your Name <dan.weibren@gmail.com>
2025-03-06 18:41:28 +0200

    Remove full hardcoded html

--- js/book.controller.js
+++ js/book.controller.js
@@ -3,15 +3,10 @@
 function onInit() {
     render()
 }
-
 function render() {
     const elBody = document.querySelector('body')
+    elBody.innerHTML = `<table> <tbody> <tr class="table-header"><th>title</th><th>price </th><th>action</th></tr></tbody></table>`
     const elTbody = document.querySelector('table')
-    elTbody.innerHTML = `<tr class="table-header">
-                         <th>title</th>
-                         <th>price </th>
-                         <th>action</th>
-                         </tr>`
     gBooks.map(book => {
         elTbody.innerHTML += `<td>book name</td>
                               <td>price$</td>
