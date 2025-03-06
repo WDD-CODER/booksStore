51684ef3814efd558c781493858d764c383e6bb3
Your Name <dan.weibren@gmail.com>
2025-03-06 17:52:43 +0200

    Create function getBook

--- js/service/book.service.js
+++ js/service/book.service.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function getBook(){
+    const gBooks = [
+        {title: 'blabla', price: '2$'},
+        {title: 'blabla', price: '2$'},
+        {title: 'blabla', price: '2$'}
+    ]
+    return gBooks
+    }
