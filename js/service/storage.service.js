10cbbbb87baaf9f6d6a6eb95fe8b44769bf7184b
Your Name <dan.weibren@gmail.com>
2025-03-10 20:39:26 +0200

    Fixed storage service

--- js/service/storage.service.js
+++ js/service/storage.service.js
@@ -1,23 +1,45 @@
 'use strict';
 
-function getBookFromLocalStorage(bookObject){
-    const book = localStorage.getItem(bookObject.id);
-    return book;
+// function getBooksFromLocalStorage(bookObject){
+//     for (let i = 0; i < localStorage.length; i++) {
+//         const book =(localStorage.getItem(bookObject.id));
+//     }
+    
+//     console.log("🚀 ~ getBookObjctFromLocalStorage ~ book:", book)
+//     return book;
+// }
+
+// function getBookObjectFromLocalStorage(bookObject){
+//    const curBookJson =(localStorage.getItem(bookObject.id))
+//    sa = fromJsonToObject(curBookJson)
+//    return curBook;
+// }
+
+
+// function removeBookFromLocalStorage(bookObject) {
+//     localStorage.removeItem(bookObject.id)
+// }
+// function updateLocalStorage() {
+//     localStorage.clear()
+//     _saveBook() //, json)
+// }
+
+
+
+// function _saveBook() {
+//     gBooks.forEach(book => {
+//         var json = fromObjectToJson(book, null, 4)
+//         localStorage.setItem(`${book.id}`, json)
+//     });
+
+// }
+///////////////////
+
+function saveToStorage(key, val) {
+    localStorage.setItem(key, JSON.stringify(val))
 }
 
-function removeBookFromLocalStorage(bookObject) {
-    localStorage.removeItem(bookObject.id)
-}
-function updateLocalStorage() {
-    localStorage.clear()
-    _saveBook() //, json)
-}
-
-
-function _saveBook() {
-    gBooks.forEach(book => {
-        var json = fromObjectToJson(book, null, 4)
-        localStorage.setItem(`${book.id}`, json)
-    });
-
+function loadFromStorage(key) {
+    const val = localStorage.getItem(key)
+    return JSON.parse(val)
 }
