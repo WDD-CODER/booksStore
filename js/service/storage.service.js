abf9761a405adcdf294462489650e455957c32b1
Your Name <dan.weibren@gmail.com>
2025-03-11 13:52:14 +0200

    Cleaned storage.service

--- js/service/storage.service.js
+++ js/service/storage.service.js
@@ -1,40 +1,5 @@
 'use strict';
 
-// function getBooksFromLocalStorage(bookObject){
-//     for (let i = 0; i < localStorage.length; i++) {
-//         const book =(localStorage.getItem(bookObject.id));
-//     }
-    
-//     console.log("🚀 ~ getBookObjctFromLocalStorage ~ book:", book)
-//     return book;
-// }
-
-// function getBookObjectFromLocalStorage(bookObject){
-//    const curBookJson =(localStorage.getItem(bookObject.id))
-//    sa = fromJsonToObject(curBookJson)
-//    return curBook;
-// }
-
-
-// function removeBookFromLocalStorage(bookObject) {
-//     localStorage.removeItem(bookObject.id)
-// }
-// function updateLocalStorage() {
-//     localStorage.clear()
-//     _saveBook() //, json)
-// }
-
-
-
-// function _saveBook() {
-//     gBooks.forEach(book => {
-//         var json = fromObjectToJson(book, null, 4)
-//         localStorage.setItem(`${book.id}`, json)
-//     });
-
-// }
-///////////////////
-
 function saveToStorage(key, val) {
     localStorage.setItem(key, JSON.stringify(val))
 }
